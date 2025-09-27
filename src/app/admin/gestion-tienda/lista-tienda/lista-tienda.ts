2788924a77cdb7a7aef5f5bc836064b1c5e18a93
YONNY <yonny.huarancca.27@unsch.edu.pe>
2025-09-27 11:22:02 -0500

    subida

---
+++ src/app/admin/gestion-tienda/lista-tienda/lista-tienda.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-tienda',
+  imports: [],
+  templateUrl: './lista-tienda.html',
+  styleUrl: './lista-tienda.css'
+})
+export class ListaTienda {
+
+}
